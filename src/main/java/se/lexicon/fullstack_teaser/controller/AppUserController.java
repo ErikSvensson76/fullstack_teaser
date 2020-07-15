5c40f2f9c3876514bdb26a344927d06492a5c943
Erik Svensson <eosvensson@gmail.com>
2020-07-15 11:55:05 +0200

    Finished for the day

--- src/main/java/se/lexicon/fullstack_teaser/controller/AppUserController.java
+++ src/main/java/se/lexicon/fullstack_teaser/controller/AppUserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import se.lexicon.fullstack_teaser.data.AppUserRepository;
 import se.lexicon.fullstack_teaser.entity.AppUser;
 
@@ -25,6 +27,23 @@ public class AppUserController {
         model.addAttribute("userList", appUsers);
         return "user_control";
     }
+
+    @GetMapping("/people/{id}")
+    public String findById(@PathVariable("id") String userId, Model model){
+        AppUser user = appUserRepository.findById(userId)
+                .orElseThrow(RuntimeException::new);
+
+        model.addAttribute("user", user);
+        return "user_view";
+    }
+
+    @PostMapping("/people/{id}/delete")
+    public String deleteById(@PathVariable("id") String userId){
+        appUserRepository.deleteById(userId);
+        return "redirect:/people";
+    }
+
+
 
 
 }
